5938685a0e04a1dfa00a9d433ae0891e3057a705
biboka <biboka@t-online.hu>
2016-11-19 23:26:37 +0100

    Added "Settings" activity. Studio default for now, but it opens and it's nice:)

--- app/src/main/java/net/biboka/transrad/transrad/activity/MainActivity.java
+++ app/src/main/java/net/biboka/transrad/transrad/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package net.biboka.transrad.transrad.activity;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -87,6 +88,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
